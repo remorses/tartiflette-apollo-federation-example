08ad6f2b3753b56f8a52dfaacfce07476063a8e6
remorses <beats.by.morse@gmail.com>
2019-09-21 12:11:39 +0200

    works

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Start with `docker-compose build && docker-compose up`
-
+Go to http://localhost:8000/ for playground
+Resolvers still not implemented
--- src1/resolvers/__init__.py
+++ src1/resolvers/__init__.py
@@ -1,6 +1,5 @@
 import os.path
 from tartiflette_scalars import Json, AnyScalar
-from prtty import pretty
 from tartiflette import Resolver, Scalar
 
 
